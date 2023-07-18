3b640d4f8c438b2db872848025f3968554f71e7e
fred nakilya <frednakilya14@gmail.com>
2023-07-18 12:39:24 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet -alphabet letters
+ *
+ * return:void
+ */
+void print_alphabet(void)
+{
+	char az;
+	for (az = 'a'; az <= 'z'; az++)
+	_putchar(az);
+	_putchar('\n');
+}	
